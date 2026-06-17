ad74abd7ba1a9754095b75fefc6ad5e379d9c1f8
trevortsui <trevortsui007@gmail.com>
2026-06-17 09:31:46 +0800

    chore(url): 本次提交更新正式服后端地址

--- vite.config.js
+++ vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
     proxy: {
       // 把所有 /api 开头的请求代理到后端
       '/api': {
-        target: 'http://localhost:31173',   // 你的后端地址
+        target: 'http://10.6.9.15:8888',   // 你的后端地址
         changeOrigin: true,                 // 必须，修改请求头中的 Origin
         rewrite: (path) => path.replace(/^\/api/, '/api'), // 可根据需要调整
         // 如果后端需要支持其他方法，可以加以下配置
